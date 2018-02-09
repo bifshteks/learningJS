35d86135297811d26c405fb1cae14162e52dae9f
Artem <artem.pif@mail.ru>
2018-02-09 16:38:56 +0300

    4

--- promise.js
+++ promise.js
@@ -4,8 +4,15 @@ var promise = new Promise(function(resolve, reject){
 	fs.readFile('./data.json', (err, data) => err ? reject(err) : resolve(data)	);
 });
 
-promise.then(data => {
-				const obj = JSON.parse(data);
-				console.log('Результат: ', obj)
-			},
-			err => console.log('Ошибка: ', err))
+
+function parseJSONData(data){
+	try {
+	  var obj = JSON.parse(data);
+	  console.log('Результат: ', obj);
+	} catch (e) {
+	  console.error('Ошибка: ', err)
+	}
+}
+
+
+promise.then(parseJSONData, err => console.log('Ошибка: ', err))
