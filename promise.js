db15ea883c54609f6dea956ba46394c89d82d600
Artem <artem.pif@mail.ru>
2018-02-09 17:09:35 +0300

    6

--- promise.js
+++ promise.js
@@ -1,21 +1,43 @@
 const fs = require('fs');
 
-const openJson = (path) => new Promise(
-	(resolve, reject) => {
-		fs.readFile(path, (err, data) => {
+// const openJson = (path) => new Promise(
+// 	(resolve, reject) => {
+// 		fs.readFile(path, (err, data) => {
+// 			if (err) {
+// 				reject(err);
+// 			}
+// 			try {
+// 				resolve(JSON.parse(data));
+// 			} catch (err) {
+// 				reject(err)
+// 			}
+// 		})
+// });
+
+
+// openJson('./data.json').then(
+// 	(obj) => console.log(obj),
+// 	(err) => console.log(err)
+// )
+
+
+
+const parseJson = (obj) => new Promise((resolve, reject) => {
+	try{
+		const data = JSON.parse(obj);
+		resolve(data)
+	} catch(e){
+		reject(e)
+	}	
+})
+const openFile = (path) => new Promise((resolve, reject) => {
+	fs.readFile(path, (err, data) => {
 			if (err) {
 				reject(err);
+			} else {
+				resolve(parseJson(data))
 			}
-			try {
-				resolve(JSON.parse(data));
-			} catch (err) {
-				reject(err)
-			}
-		})
-});
+		})	
+})
 
-
-openJson('./data.json').then(
-	(obj) => console.log(obj),
-	(err) => console.log(err)
-)
+openFile('./data.json').then((res)=> console.log(res), (err) => console.log(err))
