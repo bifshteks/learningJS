0a8703ddb746f6c5d63ae037dae1c6f43bc45407
Artem <artem.pif@mail.ru>
2018-02-11 15:48:00 +0300

    i don't know

--- myAxios.js
+++ myAxios.js
@@ -1,64 +1,135 @@
 var XMLHttpRequest = require('xhr2');
 
 
-var method = process.argv[2];
-var url = process.argv[3];
+// var method = process.argv[2];
+var url = 'https://mathiasbynens.be/demo/ip'//process.argv[3];
 
 
 function getJson(data){
 	return new Promise((resolve, reject) => {
 		try{
 			let jsonData = JSON.parse(data);
+			console.log('jsonData ', jsonData)
 			resolve(jsonData)
-			console.log('Test 4')
 		} catch (error) {
-			// console.log('Test 5', data)
-			reject(data)// reject(error, data)
+			reject([data, error])// reject(error, data)
 		}
 	});
 }
 
-function getData(url, method){
+function getData(url, method, body){
 	return new Promise((resolve, reject) => {
 		let xhr = new XMLHttpRequest();
+		xhr.responseType = 'json';
 		xhr.open(method, url);
 		xhr.onload = () => {
 			if (xhr.status === 200){
-				// let jsonData = JSON.parse(xhr.response);
-				console.log('Test 1')
 				resolve(xhr.response)
 			} else {
-				console.log('Test 2')
+				console.log('used method2 ', method)
 				reject(xhr.statusText);
 			}
 		};
 
 		xhr.onerror = (error) => {
-			console.log('Test 3')
+			console.log('used method ', method)
 			reject(error)
 		};
 
-		xhr.send();
+		xhr.send(body);
 	})
 }
 
-getData(url, method)
-	.then((resp) => {
-		// let jsonData = JSON.parse(resp);
-		console.log('Test 6')
-		return getJson(resp)
-		console.log('Test 7')
-	}, (error) => {
-		console.log('Не удалось отправить запрос. Ошибка: ', error)
-	})
-	.then((json) => {
-		console.log(json, 'Test 10')
-		console.log('Данные получены: ', json)
-	}, (error, dataNotJson) => {
-		console.error('Преобразование в Json невозможно. Данные: ', error)
-		console.log('Итоговые данные: ', dataNotJson)
-	})
-	.catch((error) => {
-		console.error('Во время выполнения возникла ошибка: ', error);
+function sendRequest(url, method, body){
+	getData(url, method, body)
+		// .then(
+		// 	(resp) => {
+		// 		console.log('retunr')
+		// 		return getJson(resp)
+		// 	},
+		// 	(error) => {
+		// 		console.error('Во время выполнения возникла ошибка: ', error);
+		// 	}
+		// )
+		// .then(
+		// 	(json) => {
+		// 		// console.log(json, 'Test 10')
+		// 		console.log('Данные получены: ', json)
+		// 	}, 
+		// 	(dataNotJson) => {
+		// 		console.error('Преобразование в Json невозможно. Данные: ', dataNotJson)
+		// 		// console.log('Итоговые данные: ', dataNotJson)
+		// })
+		.then(
+			(resp) => {
+				console.log('retunr')
+				return getJson(resp)
+					.then(
+						(json) => {
+							// console.log(json, 'Test 10')
+							console.log('Данные получены: ', json)
+						},
+						(data) => {
+							console.log('Преобразование в Json невозможно. Данные ', data[0], 'error: ',data[1])
+						}
+					
+					)
+			}
+		)
+		.catch((error) => {
+			console.error('Во время выполнения возникла ошибка: ', error);
+		})
+		// .then(
+		// 	(json) => {
+		// 		// console.log(json, 'Test 10')
+		// 		console.log('Данные получены: ', json)
+		// 	},
+		// 	(data) => {
+		// 		console.log('Преобразование в Json невозможно. Данные ', data.slice(0, 30))
+		// 	}
+		
+		// )
+		
+		// .catch((error) => {
+		// 	console.error('Во время выполнения возникла ошибка: ', error);
+		// })
+}
+function Request(url, body=''){
+	this.method = 'GET'
+	this.url = url
+	this.body = body
 
-	})
+	var self = this;
+
+	this.get = () => {
+		sendRequest(this.url, this.method, this.body)
+	}
+
+	this.post = () => {
+		this.method = 'POST'
+		sendRequest(this.url, this.method, this.body)
+	}
+
+	this.patch = () => {
+		this.method = 'PATCH'
+		sendRequest(this.url, this.method, this.body)
+	}
+
+	this.put = () => {
+		this.method = 'PUT'
+		sendRequest(this.url, this.method, this.body)
+	}
+
+	this.delete = () => {
+		this.method = 'DELETE'
+		sendRequest(this.url, this.method, this.body)
+	}
+
+	this.head = () => {
+		this.method = 'HEAD'
+		sendRequest(this.url, this.method, this.body)
+	}
+}
+
+var newRequest = new Request(url);
+newRequest.get()
